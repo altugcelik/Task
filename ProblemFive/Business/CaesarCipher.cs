9821d9be7b51eb1294d3788ad85f793d561d0c7c
Maho PC <Maho PC@DESKTOP-CKGNBNK>
2018-09-01 20:29:01 +0300

    ProblemFive push

---
+++ ProblemFive/Business/CaesarCipher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProblemFive.Business
+{
+    public static class CaesarCipher
+    {
+        public static string Encipher(string input, int key)
+        {
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += cipher(ch, key);
+
+            return output;
+        }
+
+        public static string Decipher(string input, int key)
+        {
+            return Encipher(input, 29 - key);
+        }
+
+        public static char cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 29) + d);
+        }
+    }
+}
---
+++ ProblemFive/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ProblemFive.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProblemFive.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string password = "Burak";
+          
+
+            
+            int key = Convert.ToInt32(-2);
+         
+
+            string cipherText = CaesarCipher.Encipher(password, key);
+
+            string t = CaesarCipher.Decipher(cipherText, key);
+
+
+            string burak = "";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EncryptionFile(HttpPostedFileBase file, int index, string password)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DecryptionFile(HttpPostedFileBase file, int index, string password)
+        {
+            return View();
+        }
+    }
+}
